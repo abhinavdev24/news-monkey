0df8a48524e0302af00f69590a67339d28754aef
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2022-12-28 21:24:58 +0530

    Added spinner for loading screen

--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
     };
   }
 
   async componentDidMount() {
+    this.setState({ loading: true });
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a1a23064c6c6460184091e5767cf1683&pageSize=20&page=${this.state.page}`;
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -19,10 +21,12 @@ export class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalArticles: parsedData.totalResults,
+      loading: false,
     });
   }
 
   handlePrevClick = async () => {
+    this.setState({ loading: true });
     console.log("Previous");
     if (this.state.page > 1) {
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a1a23064c6c6460184091e5767cf1683&pageSize=20&page=${
@@ -34,11 +38,13 @@ export class News extends Component {
       this.setState({
         page: this.state.page - 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
 
   handleNextClick = async () => {
+    this.setState({ loading: true });
     console.log("Next");
     if (this.state.page + 1 <= Math.ceil(this.state.totalArticles / 20)) {
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a1a23064c6c6460184091e5767cf1683&pageSize=20&page=${
@@ -50,6 +56,7 @@ export class News extends Component {
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -58,22 +65,26 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row justify-content-between">
-          {this.state.articles.map((newsElement) => {
-            return (
-              <NewsItem
-                key={newsElement.url}
-                url={newsElement.url}
-                title={newsElement.title ? newsElement.title.slice(0, 45) : ""}
-                description={
-                  newsElement.description
-                    ? newsElement.description.slice(0, 88)
-                    : ""
-                }
-                imageUrl={newsElement.urlToImage}
-              />
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((newsElement) => {
+              return (
+                <NewsItem
+                  key={newsElement.url}
+                  url={newsElement.url}
+                  title={
+                    newsElement.title ? newsElement.title.slice(0, 45) : ""
+                  }
+                  description={
+                    newsElement.description
+                      ? newsElement.description.slice(0, 88)
+                      : ""
+                  }
+                  imageUrl={newsElement.urlToImage}
+                />
+              );
+            })}
         </div>
         <div className="d-flex justify-content-between">
           <button
