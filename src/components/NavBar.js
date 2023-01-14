b60d45dc37e46f1639e3b69d4b92bf76aff9ff33
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2023-01-14 21:20:51 +0530

    fixed top

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             NewsMonkey
